c9ea6939514b5e7a06973daa3d67b95b0580edbb
Richard Pitts <richardepitts@gmail.com>
2019-07-14 18:18:20 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeliLine = []
+
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine === o) {
+    "The line is currently empty."
+  } else {
+    return "The line is currently: 1. Ada, 2. Grace"
+  }
+}
