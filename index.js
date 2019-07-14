ca5bcfc48b36be27cb59eaaf6903812ec78787b8
Richard Pitts <richardepitts@gmail.com>
2019-07-14 18:31:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ function currentLine(katzDeliLine) {
     i++;
   }
 }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
 
 
 
@@ -35,14 +40,9 @@ function currentLine(katzDeliLine) {
 
 
   
-  function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
-}
+  // function currentLine(katzDeli) {
+  // let i = 0;
+  // while (i < katzDeli.length) {
+  //   line.push(` `+[i+1]+`. `  + katzDeli[i])
+  //   i++;
+  // }
