f5c69c5bcda3bbcc4e2ba4777416df3afd10add9
Richard Pitts <richardepitts@gmail.com>
2019-07-14 18:22:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ function currentLine(katzDeliLine) {
   }
 }
 
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+
 function nowServing(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
