78a7227ad1eadaf216656b494ea985fb1da1755f
Richard Pitts <richardepitts@gmail.com>
2019-07-14 18:24:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var katzDeliLine = []
 
 function takeANumber(katzDeliLine, name) {
-  
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+
+function nowServing(katzDeliLine)
+
 
 
 
