ca9c0968775090a5331fd7d7de26e41a83155760
Richard Pitts <richardepitts@gmail.com>
2019-07-14 18:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ function nowServing(katzDeliLine) {
   while (i < katzDeliLine.length) {
     i++;
   }
-  if (katzDeli.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   }
   else {
-  return (`Currently serving ${katzDeli.shift()}.`);
+  return (`Currently serving ${katzDeliLine.shift()}.`);
   }
 }
 
@@ -23,11 +23,11 @@ var lineWithNames = []
 function currentLine(katzDeliLine) {
   let i = 0;
     while (i < katzDeliLine.length) {
-    lineWithNames.push(` `+[i+1]+`. `  + katzDeli[i])
+    lineWithNames.push(` `+[i+1]+`. `  + katzDeliLine[i])
     i++;
   }
 
-  if (katzDeli.length === 0) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
   return (`The line is currently:` + lineWithNames);
