cc461cf6ec28ae8cdc31de473b6acaa56ec7271a
Richard Pitts <richardepitts@gmail.com>
2019-07-14 18:22:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function currentLine(katzDeliLine) {
     return "The line is currently: 1. Ada, 2. Grace"
   }
 }
+
+function nowServing(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
